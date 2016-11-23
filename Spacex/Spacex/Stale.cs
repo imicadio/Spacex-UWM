1e3050c52a1d7e93318f34f35dbdc96df50f3cbd
imicadio <mrjesio@gmail.com>
2016-11-23 22:56:17 +0100

    Tytuł i Stałe

---
+++ Spacex/Spacex/Stale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spacex
+{
+    public class Stale
+    {
+        public static int GRA_SZEROKOSC = 400;
+        public static int GRA_WYSOKOSC = 600;
+        public static string GRA_TYTUL = "Spacex";
+    }
+}
