80504bfccc61f7b271e2e0ae357d0e46e73540b8
imicadio <mrjesio@gmail.com>
2016-11-23 23:10:32 +0100

    Dodałem Screen

---
+++ Spacex/Spacex/Screens/GameScreen.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spacex.Screens
+{
+    class GameScreen : Screen
+    {
+    }
+}
---
+++ Spacex/Spacex/Screens/Screen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spacex.Screens
+{
+    public abstract class Screen
+    {
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
