bca6cf7b11beaee8a5769aeb6370ca8a647c8bfd
imicadio <mrjesio@gmail.com>
2016-11-23 23:43:45 +0100

    Dodałem kolumny

--- Spacex/Spacex/Obrazki/kolumny.cs
+++ Spacex/Spacex/Obrazki/kolumny.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +9,24 @@ namespace Spacex.Obrazki
 {
     public class kolumny
     {
+        public Texture2D tekstura;
+        public Vector2 Pozycja;
+
+
+        public kolumny()
+        {
+            this.tekstura = Stale.CONTENT.Load<Texture2D>("Tekstury/kolumny");
+            this.Pozycja = new Vector2(250, 0);
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+            Stale.SPRITEBATCH.Draw(this.tekstura, this.Pozycja, Color.White);
+        }
     }
 }
--- Spacex/Spacex/Screens/GameScreen.cs
+++ Spacex/Spacex/Screens/GameScreen.cs
@@ -12,7 +12,7 @@ namespace Spacex.Screens
         public Texture2D tlo;
         public Obrazki.statek statek;
         public Obrazki.scroll scroll;
-
+        public List<Obrazki.kolumny> kolumny;
 
         public GameScreen()
         {
@@ -24,12 +24,20 @@ namespace Spacex.Screens
             tlo = Stale.CONTENT.Load<Texture2D>("Tekstury/tlo");
             statek = new Obrazki.statek();
             scroll = new Obrazki.scroll();
+            kolumny = new List<Obrazki.kolumny>();
+            kolumny.Add(new Obrazki.kolumny());
+
 
             base.LoadContent();
         }
 
         public override void Update()
         {
+            foreach (var item in kolumny)
+            {
+                item.Update();
+            }
+
             statek.Update();
             scroll.Update();
             base.Update();
@@ -41,6 +49,12 @@ namespace Spacex.Screens
 
             Stale.SPRITEBATCH.Draw(this.tlo, Vector2.Zero, Color.White);
             scroll.Draw();
+
+            foreach (var item in kolumny)
+            {
+                item.Draw();
+            }
+
             statek.Draw();
 
 
